2a8a3e65ccfb5bbe1df8733b6a72fdeb0944d9af
kkesid <160251018+kkesid@users.noreply.github.com>
2024-09-12 00:53:59 +0300

    Наследование. Исключительные ситуации

---
+++ HW6/HW6_ex1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+
+class NullException : public std::exception {
+public:
+    NullException() : std::exception("Ошибка: Деление на 0!") {}
+};
+
+class OutOfRange : public std::exception {
+public:
+    OutOfRange() : std::exception("Ошибка: Выход за пределы диапазона!") {}
+};
+
+double calculate_f(double x, double N, int k) {
+    if (x < 0 || x > k) {
+        throw OutOfRange();
+    }
+    if (x == N) {
+        throw NullException();
+    }
+    return 1 / (x - N);
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    double N = 10;
+    int k;
+
+    std::cout << "Введите k (до 20): ";
+    std::cin >> k;
+
+    if (k < 0 || k > 20) {
+        std::cerr << "Ошибка: значение k должно быть в диапазоне от 0 до 20." << std::endl;
+        return 1;
+    }
+
+    std::vector<double> f(k + 1);
+
+    for (int x = 0; x <= k; ++x) {
+        try {
+            f[x] = calculate_f(x, N, k);
+            std::cout << "f[" << x << "] = " << f[x] << std::endl;
+        }
+        catch (const  NullException& e) {
+            std::cerr << "При x = " << x << ": " << e.what() << std::endl;
+        }
+        catch (const OutOfRange& e) {
+            std::cerr << "При x = " << x << ": " << e.what() << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Произошла непредвиденная ошибка: " << e.what() << std::endl;
+        }
+    }
+
+    return 0;
+}
---
+++ HW6/HW6_ex2.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+class NullException : public std::exception {
+public:
+    NullException() : std::exception("Ошибка: Деление на ноль") {}
+};
+
+class InvalidInput : public std::exception {
+public:
+    InvalidInput() : std::exception("Ошибка: Ввод должен быть целым числом") {}
+};
+
+class OutOfBounds : public std::exception {
+public:
+    OutOfBounds() : std::exception("Ошибка: Выход за границы массива") {}
+};
+
+class InvalidType : public std::exception {
+public:
+    InvalidType() : std::exception("Ошибка: Неверный тип входных параметров") {}
+};
+
+double division(int num, int den) {
+    if (den == 0) {
+        throw NullException();
+    }
+    return (double)num / den;
+}
+
+int getInput() {
+    int number;
+    cout << "Введите целое число: ";
+    if (!(cin >> number)) {
+        throw InvalidInput();
+    }
+    return number;
+}
+
+void matrixElement(const vector<vector<int>>& matrix, int row, int col) {
+    if (row < 0 || row >= matrix.size() || col < 0 || col >= matrix[0].size()) {
+        throw OutOfBounds();
+    }
+
+    cout << "Элемент матрицы [" << row << "][" << col << "] = " << matrix[row][col] << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+   vector<vector<int>> matrix = {
+        {1, 2},
+        {3, 4}
+    };
+
+    try {
+        cout << "ПРИМЕР ДЕЛЕНИЯ: " << endl;
+        int numerator = getInput();
+        int denominator = getInput();
+        cout << "Результат деления: " << division(numerator, denominator) << endl;
+
+        cout << endl << "---------------------------------------------" << endl << endl;
+
+        cout << "ПРИМЕР ДОСТУПА К ЭЛЕМЕНТУ МАТРИЦЫ: " << endl;
+        int row = getInput();
+        int col = getInput();
+        matrixElement(matrix, row, col);
+    }
+    catch (const NullException& e) {
+        std::cerr << e.what() << endl;
+    }
+    catch (const InvalidInput& e) {
+        std::cerr << e.what() << endl;
+    }
+    catch (const OutOfBounds& e) {
+        std::cerr << e.what() << endl;
+    }
+    catch (const InvalidType& e) {
+        std::cerr << e.what() << endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Произошла ошибка: " << e.what() << endl;
+    }
+
+    return 0;
+}
